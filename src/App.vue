286a704141ad8f54502e13feaf8991228eeea9ac
Cristianr <calrod350@gmail.com>
2021-09-20 07:34:56 -0500

    actualizando entorno de desasrrollo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Headerbar> </Headerbar>
+   <router-view></router-view>
+   <!-- <Login></Login> -->
+   <!-- <Panelmain> </Panelmain> -->
+   <Footer> </Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Headerbar from './components/Headerbar.vue';
+// import Login from './components/Login.vue';
+// import Panelmain from './components/Panelmain.vue';
+import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Headerbar,
+    // Login,
+    // Panelmain
+    Footer
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* margin: 0%;
+  padding: 0%;
+  width: 100%;
+  height: 100%; */
 }
+
 </style>
